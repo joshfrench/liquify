2e57934ee1d6c0c2a02a5532acf079c46a4bf58a
Josh French <josh@vitamin-j.com>
2009-09-10 23:06:07 -0400

    Add parts/parts drop

--- lib/liquify/page_drop.rb
+++ lib/liquify/page_drop.rb
@@ -1,4 +1,19 @@
 module Liquify
+
+  class PartDrop < Liquid::Drop
+    def initialize(page)
+      @page = page
+    end
+
+    def [](part)
+      if part = @page.part(part)
+        part.content
+      end
+    end
+
+    alias method_missing []
+  end
+
   class PageDrop < Liquid::Drop
     attr_accessor :page
     delegate :title, :slug, :breadcrumb, :to => :page
@@ -11,6 +26,10 @@ module Liquify
       if author = @page.created_by
         author.name
       end
+    end
+
+    def parts
+      PartDrop.new(@page)
     end
   end
 end
